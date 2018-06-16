947a4ab5882137b3caf0109e1b34d58f30c2476b
Michael Ball <mike@ledgerdev.com>
2018-06-15 19:12:24 -0700

    A few thoughts on timelines

--- driver/syntax.sql
+++ driver/syntax.sql
@@ -11,28 +11,66 @@ from person
   where shirt/color = `blue`
   select entity/id, person/name
 
+/* The from syntax implies that entities have a type or class(aka tables).
+   Not sure if this is good or bad? */
+
+where shirt/color == `blue` and
+      entity/type == 'person'
+    select entity/id, person/name
+
+-- so entity/id and entity/type are system properties
+
+
+-- MUST have a way to specify point in time for queries
+from person
+  where shirt/color = `blue`
+  select entity/id, person/name
+  asof '2018-06-15'
+
+
+/* probably should have a notion of multiple timelines
+   domain time and system time */
+from person
+  where shirt/color = `blue`
+  select entity/id, person/name
+  asof-systime '2018-06-15'
+
+
+
 
 select shirt/color, (count entity/id)
 
 
-# How would we assert facts?
-
+-- How would we assert facts?
 on id=101 assert
     person/name = `bob`,
     shirt/color = `blue`
 
-
-# assert a new user and roles
+--  assert a new user and roles
 on new assert
     user/name = `bob`,
     user/roles = [11 22]
 
+--  assert a new user and roles and specify the entity type?
+on new person assert
+    user/name = `bob`
 
 
-# upsert syntax?
-# Is upsert even necessary because at the core if you state a fact that is
-# the same as it presently is, shouldn't it just be ignored?
-# probably need a re-assert statement
+/* do we need to be able to specify domain time 
+   for assertations? If there's a fact that we now know,
+   but which was in existance or true at some earlier point. */
+on id=101 assert
+    person/name = `bob`,
+    shirt/color = `blue`
+    asof '2018-06-15' 
+
+
+
+
+/* upsert syntax?
+   Is upsert even necessary because at the core if you state a fact that is
+   the same as it presently is, shouldn't it just be ignored?
+   probably need a re-assert statement */
 on id=101 else new assert
     user/name = `bob`
 
@@ -40,20 +78,14 @@ on id=101 else new assert
 
 
 
-# How to retract a fact?
+-- How to retract a fact
 on id = 101 retract
     person/name, shirt/color
 
 
 
+-- How to forget a fact
+on id = 101 forget 
+            person/name
 
-# How to forget a fact? Perhaps delete is better term? Excision?
-on id = 101 excise
-  person/name
-
-  on id = 101 forget
-    person/name
-
-  on id = 101 erase
-    person/name
 
